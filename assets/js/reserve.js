a2702b703b2186f2c62fbe6b18dc634143be2d18
bf_tsai <tsai19911026@gmail.com>
2023-11-02 15:33:57 +0800

    feat: 🎸 add conversation back to reserve

--- assets/js/reserve.js
+++ assets/js/reserve.js
@@ -1,4 +1,5 @@
 //jQuery
+//seitch reserveMyDicuss reserveManageAppointments
 $(document).ready(function () {
     $('.btn-group .btn').click(function (e) { 
         e.preventDefault();
@@ -14,7 +15,9 @@ const reserveContentAllLi=[...document.querySelectorAll('.reserve-content li')];
 const reserveMyDicuss=document.querySelector('.reserve-myDicuss');
 const reserveManageAppointments=document.querySelector('.reserve-manageAppointments');
 const pagination=document.querySelector('.pagination');
+const backToReserveAll=[...document.querySelectorAll('.backToReserve')];
 
+//reserve into reserveMyDicuss or reserveManageAppointments
 reserveContentAllLi.forEach(item=>{
     item.addEventListener('click',e=>{
         let stateDom=[...reserveBtnGroup.children[0].children[0].children[0].children].find(item=>{
@@ -22,7 +25,6 @@ reserveContentAllLi.forEach(item=>{
                 return item;
             }
         });
-        console.log(stateDom.textContent);
         if(stateDom.textContent==='我的預約'){
             reserveBtnGroup.classList.add('opacity-0');
             reserveContent.classList.add('opacity-0');
@@ -37,11 +39,48 @@ reserveContentAllLi.forEach(item=>{
                 }, 0);
             }, 400);
         }else{
-            reserveBtnGroup.classList.add('d-none');
-            reserveContent.classList.add('d-none');
-            pagination.classList.add('d-none');
-            reserveManageAppointments.classList.remove('d-none');
+            reserveBtnGroup.classList.add('opacity-0');
+            reserveContent.classList.add('opacity-0');
+            pagination.classList.add('opacity-0');
+            setTimeout(() => {
+                reserveBtnGroup.classList.add('d-none');
+                reserveContent.classList.add('d-none');
+                pagination.classList.add('d-none');
+                reserveManageAppointments.classList.remove('d-none');
+                setTimeout(() => {
+                    reserveManageAppointments.classList.remove('opacity-0');
+                }, 0);
+            }, 400);
         }
-       
     })
-})
+});
+//reserveMyDicuss back to reserve
+backToReserveAll[0].addEventListener('click',e=>{
+    reserveMyDicuss.classList.add('opacity-0');
+    setTimeout(() => {
+        reserveBtnGroup.classList.remove('d-none');
+        reserveContent.classList.remove('d-none');
+        pagination.classList.remove('d-none');
+        reserveMyDicuss.classList.add('d-none');
+        setTimeout(() => {
+            reserveBtnGroup.classList.remove('opacity-0');
+            reserveContent.classList.remove('opacity-0');
+            pagination.classList.remove('opacity-0');
+        }, 0);
+    }, 400);
+});
+//reserveManageAppointments back to reserve
+backToReserveAll[1].addEventListener('click',e=>{
+    reserveManageAppointments.classList.add('opacity-0');
+    setTimeout(() => {
+        reserveBtnGroup.classList.remove('d-none');
+        reserveContent.classList.remove('d-none');
+        pagination.classList.remove('d-none');
+        reserveManageAppointments.classList.add('d-none');
+        setTimeout(() => {
+            reserveBtnGroup.classList.remove('opacity-0');
+            reserveContent.classList.remove('opacity-0');
+            pagination.classList.remove('opacity-0');
+        }, 0);
+    }, 400);
+});
