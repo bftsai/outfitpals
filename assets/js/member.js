0e8bc4ca9cbfbc8d4d3d96588475fe544a17744e
bf_tsai <tsai19911026@gmail.com>
2023-11-16 01:19:12 +0800

    docs: ✏️ add 5-1-1 and google third party signUp

--- assets/js/member.js
+++ assets/js/member.js
@@ -6,6 +6,11 @@
 
 */
 
+//member pages
+const memberIndex=document.querySelector('.member-index');
+const memberIndexForm=document.querySelector('.memberIndexForm');
+const memberSignUpData=document.querySelector('.member-signUpData');
+const memberSignUpForm=document.querySelector('.memberSignUpData');
 //signIn signUp Btn
 const signInBtn=document.querySelector('.signInBtn');
 const signUpBtn=document.querySelector('.signUpBtn');
@@ -15,7 +20,8 @@ const gmailSignUp=document.querySelector('.gmailSignUp');
 const account=document.getElementById('account');
 const pwd=document.getElementById('pwd');
 //member signUp input
-const memberSignUpData=document.querySelector('.memberSignUpData');
+const profileImage=document.getElementById('profileImage'); //input
+let profilePhoto=document.querySelector('.profilePhoto'); //image
 const signUpPwd=document.getElementById('signUpPwd');
 const signUpMail=document.getElementById('signUpMail');
 const signUpName=document.getElementById('signUpName');
@@ -36,6 +42,7 @@ const signUpIntroduce=document.getElementById('signUpIntroduce');
 
 //submit 
 const memberIndexSubmit=document.querySelector('.memberIndexSubmit');
+const memberIndexCancel=document.querySelector('.memberIndexCancel');
 const memberSignUpSubmit=document.querySelector('.memberSignUpSubmit');
 //signIn validation
 function checkSignInForm(){
@@ -63,20 +70,68 @@ function checkSignInForm(){
         }, false);
     });
 };
+//axios
+//json-server
+const apiUrl='http://localhost:3000/';
+const ajax={
+    async register(obj){
+        try {
+            const register=await axios.post(`${apiUrl}register`,obj);
+            localStorage.outfitpalsToken=register.data.accessToken;
+            if(register.status==201){
+                signUpMail.value=account.value;
+                signUpPwd.value=pwd.value;
+                memberIndex.classList.add('opacity-0');
+                setTimeout(() => {
+                    account.value='';
+                    pwd.value='';
+                    memberIndex.classList.add('d-none');
+                    memberSignUpData.classList.remove('d-none');
+                    setTimeout(() => {
+                        memberSignUpData.classList.remove('opacity-0')
+                    }, 0);
+                }, 400);
+            }
+        } catch (err) {
+            console.log(err.response);
+            memberIndexForm.classList.add('was-validated');
+            account.classList.add('is-invalid');
+            account.nextElementSibling.textContent=err.response.data;
+            account.setAttribute("style","border-color: var(--bs-form-invalid-border-color);background-image: url('../assets/images/member/invalid.png');background-repeat: no-repeat;background-position: right calc(0.375em + 0.1875rem) center;background-size: calc(0.75em + 0.375rem) calc(0.75em + 0.375rem);");
+        }
+    },
+    async patchUsers(obj){
+        let id=0;
+        const getUsers=(await axios.get(`${apiUrl}users`)).data;
+        getUsers.forEach(item=>{
+            if(item.email===obj.email){
+                id=item.id;
+            }
+        });
+        const patchUsers=await axios.patch(`${apiUrl}users/${id}`,obj)
+    },
+    async signIn(){
+        const signIn=await axios.post(`${apiUrl}signin`,obj);
+    },
+    async delete(id){
+        try {
+            const deleted=await axios.delete(`${apiUrl}users/${id}`);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+};
 //check validation tool
 const checkSign={
     regexp:new RegExp(''),
-    checkMail(str){
+    checkAccount(str){
         this.regexp=new RegExp('^[\\w\\d-]+\\@[\\d\\w]{3,}\\.[\\d\\w]{3,}(\\.[\\d\\w]{2,})?$');
-        console.log(this.regexp);
-        console.log(str);
-        console.log(this.regexp.test(str));
         if(str===''){
             account.nextElementSibling.textContent=`請輸入密碼`;
             return false;
         }else if(!(this.regexp.test(str))){
             account.classList.add('is-invalid');
-            account.nextElementSibling.textContent=`格式錯誤，電子郵件不包含特殊字元（如：!、@、#、$、%、^）`;
+            account.nextElementSibling.textContent=`格式錯誤，電子郵件不包含特殊字元（如：!、#、$、%、^）`;
             account.setAttribute("style","border-color: var(--bs-form-invalid-border-color);background-image: url('../assets/images/member/invalid.png');background-repeat: no-repeat;background-position: right calc(0.375em + 0.1875rem) center;background-size: calc(0.75em + 0.375rem) calc(0.75em + 0.375rem);");
             return false;
         }else{
@@ -85,9 +140,40 @@ const checkSign={
             return true;
         }
     },
+    checkSignUpIndexPwd(str){
+        this.regexp=new RegExp('^[A-Z][\\d\\w]{7,}$');
+        if(str===''){
+            pwd.nextElementSibling.textContent=`請輸入密碼`;
+            return false;
+        }else if(!(this.regexp.test(str))){
+            pwd.classList.add('is-invalid');
+            pwd.nextElementSibling.textContent=`格式錯誤，密碼第一個為大寫英文，不包含特殊字元（如：!、@、#、$、%、^），最少 8 位英數字`;
+            pwd.setAttribute("style","border-color: var(--bs-form-invalid-border-color);background-image: url('../assets/images/member/invalid.png');background-repeat: no-repeat;background-position: right calc(0.375em + 0.1875rem) center;background-size: calc(0.75em + 0.375rem) calc(0.75em + 0.375rem);");
+            return false;
+        }else{
+            pwd.classList.remove('is-invalid');
+            pwd.setAttribute('style','border-color: var(--bs-form-valid-border-color);background-image: url(../assets/images/member/valid.png);background-repeat: no-repeat;background-position: right calc(0.375em + 0.1875rem) center;background-size: calc(0.75em + 0.375rem) calc(0.75em + 0.375rem);');
+            return true;
+        }
+    },
+    checkMail(str){
+        this.regexp=new RegExp('^[\\w\\d-]+\\@[\\d\\w]{3,}\\.[\\d\\w]{3,}(\\.[\\d\\w]{2,})?$');
+        if(str===''){
+            signUpMail.nextElementSibling.textContent=`請輸入密碼`;
+            return false;
+        }else if(!(this.regexp.test(str))){
+            signUpMail.classList.add('is-invalid');
+            signUpMail.nextElementSibling.textContent=`格式錯誤，電子郵件不包含特殊字元（如：!、#、$、%、^）`;
+            signUpMail.setAttribute("style","border-color: var(--bs-form-invalid-border-color);background-image: url('../assets/images/member/invalid.png');background-repeat: no-repeat;background-position: right calc(0.375em + 0.1875rem) center;background-size: calc(0.75em + 0.375rem) calc(0.75em + 0.375rem);");
+            return false;
+        }else{
+            signUpMail.classList.remove('is-invalid');
+            signUpMail.setAttribute('style','border-color: var(--bs-form-valid-border-color);background-image: url(../assets/images/member/valid.png);background-repeat: no-repeat;background-position: right calc(0.375em + 0.1875rem) center;background-size: calc(0.75em + 0.375rem) calc(0.75em + 0.375rem);');
+            return true;
+        }
+    },
     checkPwd(str){
         this.regexp=new RegExp('^[A-Z][\\d\\w]{7,}$');
-        console.log(this.regexp);
         if(str===''){
             pwd.nextElementSibling.textContent=`請輸入密碼`;
             return false;
@@ -119,7 +205,7 @@ const checkSign={
         }
     },
     checkNickName(str){
-        this.regexp=new RegExp('^[\u4e00-\u9fa5_a-zA-Z\\d\x20]{3,}$');
+        this.regexp=new RegExp('^[\u4e00-\u9fa5_a-zA-Z\\d\x20]{1,}$');
         if(str===''){
             signUpNickName.nextElementSibling.textContent=`請輸入暱稱`;
             return false;
@@ -185,72 +271,82 @@ const checkSign={
 }
 //signUp validation
 function signUpValidation() {  
-    'use strict'
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.needs-validation');
-    // Loop over them and prevent submission
-    Array.from(forms).forEach(form => {
-        form.addEventListener('submit', event => {
-            event.preventDefault();
-            event.stopPropagation();
-            form.classList.add('was-validated');
+    
+    memberSignUpForm.classList.add('was-validated');
 
-            signUpName.addEventListener('keyup',e=>{
-                if(checkSign.checkName(e.target.value)){
+    let memberObj={};
 
-                };
-            });
-            signUpName.addEventListener('paste',e=>{
-                if(checkSign.checkName(e.target.value)){
-
-                };
-            });
-
-            signUpNickName.addEventListener('keyup',e=>{
-                if(checkSign.checkNickName(e.target.value)){
-
-                };
-            });
-            signUpNickName.addEventListener('paste',e=>{
-                if(checkSign.checkNickName(e.target.value)){
-
-                };
-            });
-
-            signUpBirth.addEventListener('keyup',e=>{
-                if(checkSign.checkBirth(e.target.value)){
-
-                };
-            });
-            signUpBirth.addEventListener('paste',e=>{
-                if(checkSign.checkBirth(e.target.value)){
-
-                };
-            });
-
-            signUpTel.addEventListener('keyup',e=>{
-                if(checkSign.checkTel(e.target.value)){
-
-                };
-            });
-            signUpTel.addEventListener('paste',e=>{
-                if(checkSign.checkTel(e.target.value)){
-
-                };
-            });
-
-            signUpReservationLocation.addEventListener('keyup',e=>{
-                if(checkSign.checkReservationLocation(e.target.value)){
-
-                };
-            });
-            signUpReservationLocation.addEventListener('paste',e=>{
-                if(checkSign.checkReservationLocation(e.target.value)){
-
-                };
-            });
-        }, false);
+    signUpName.addEventListener('keyup',e=>{
+        checkSign.checkName(e.target.value);
     });
+    signUpName.addEventListener('paste',e=>{
+        checkSign.checkName(e.target.value);
+    });
+
+    signUpNickName.addEventListener('keyup',e=>{
+        checkSign.checkNickName(e.target.value);
+    });
+    signUpNickName.addEventListener('paste',e=>{
+        checkSign.checkNickName(e.target.value);
+    });
+
+    signUpBirth.addEventListener('keyup',e=>{
+        checkSign.checkBirth(e.target.value);
+    });
+    signUpBirth.addEventListener('paste',e=>{
+        checkSign.checkBirth(e.target.value);
+    });
+
+    signUpMail.addEventListener('keyup',e=>{
+        checkSign.checkMail(e.target.value);
+    });
+    signUpMail.addEventListener('paste',e=>{
+        checkSign.checkMail(e.target.value);
+    });
+
+    signUpTel.addEventListener('keyup',e=>{
+        checkSign.checkTel(e.target.value);
+    });
+    signUpTel.addEventListener('paste',e=>{
+        checkSign.checkTel(e.target.value);
+    });
+
+    signUpReservationLocation.addEventListener('keyup',e=>{
+        checkSign.checkReservationLocation(e.target.value);
+    });
+    signUpReservationLocation.addEventListener('paste',e=>{
+        checkSign.checkReservationLocation(e.target.value);
+    });
+    
+    if(checkSign.checkName(signUpName.value)&&checkSign.checkNickName(signUpNickName.value)&&checkSign.checkBirth(signUpBirth.value)&&checkSign.checkTel(signUpTel.value)&&checkSign.checkReservationLocation(signUpReservationLocation.value)&&memberSignUpForm.checkValidity()){
+        console.log('all check');
+        memberObj.image=profilePhoto.getAttribute('src');
+        memberObj.name=signUpName.value;
+        memberObj.password=signUpPwd.value;
+        memberObj['nick name']=signUpNickName.value;
+        memberObj.birthday=signUpBirth.value;
+        memberObj.tel=signUpTel.value;
+        memberObj.email=signUpMail.value;
+        memberObj['reservation time']=signUpReservationTime.value;
+        memberObj['reservation location']=signUpReservationLocation.value;
+        memberObj.height=signUpHeight.value;
+        memberObj.weight=signUpWeight.value;
+        memberObj.PopArea=signUpPopArea.value;
+        memberObj.style=signUpStyle.value;
+        memberObj['outfit price']=signUpOutfitPrice.value;
+        memberObj['love store']=signUpLoveStore.value;
+        memberObj.introduce=signUpIntroduce.value;
+        if(signUpMale.checked===true){
+            memberObj.sex='male';
+        }else{
+            memberObj.sex='female';
+        }
+        ajax.patchUsers(memberObj);
+        //https://bftsai.github.io/outfitpals/index.html
+        location.href='http://localhost:5173/outfitpals/pages/index.html';
+    }else{
+        console.log('all false');
+    }
 };
 
 checkSignInForm();
@@ -259,7 +355,7 @@ signInBtn.addEventListener('click',e=>{
         item.classList.remove('active');
     });
     memberIndexSubmit.textContent='登入';
-    
+    /*
     'use strict'
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
     const forms = document.querySelectorAll('.needs-validation');
@@ -285,6 +381,7 @@ signInBtn.addEventListener('click',e=>{
 
         }, false);
     });
+    */
 
     e.target.classList.add('active');
     gmailSignUp.classList.add('opacity-0');
@@ -300,32 +397,34 @@ signUpBtn.addEventListener('click',e=>{
     [...e.target.offsetParent.children].forEach(item=>{
         item.classList.remove('active');
     });
+    account.value='';
+    pwd.value='';
     memberIndexSubmit.textContent='註冊';
     
-    'use strict'
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.needs-validation');
-    // Loop over them and prevent submission
-    Array.from(forms).forEach(form => {
-        form.addEventListener('submit', event => {
-            event.preventDefault();
-            event.stopPropagation();
-            form.classList.add('was-validated');
-            
-            account.addEventListener('keyup',e=>{
-                checkSign.checkMail(e.target.value);
-            });
-            account.addEventListener('paste',e=>{
-                checkSign.checkMail(e.target.value);
-            });
-            pwd.addEventListener('keyup',e=>{
-                checkSign.checkPwd(e.target.value);
-            });
-            pwd.addEventListener('paste',e=>{
-                checkSign.checkPwd(e.target.value);
-            });
+    memberIndexSubmit.addEventListener('click',e=>{
+        e.preventDefault();
+        e.stopPropagation();
+        memberIndexForm.classList.add('was-validated');
 
-        }, false);
+        account.addEventListener('keyup',e=>{
+            checkSign.checkAccount(e.target.value);
+        });
+        account.addEventListener('paste',e=>{
+            checkSign.checkAccount(e.target.value);
+        });
+        pwd.addEventListener('keyup',e=>{
+            checkSign.checkSignUpIndexPwd(e.target.value);
+        });
+        pwd.addEventListener('paste',e=>{
+            checkSign.checkSignUpIndexPwd(e.target.value);
+        });
+        if(checkSign.checkAccount(account.value)&&checkSign.checkPwd(pwd.value)){
+            let obj={};
+            obj.email=account.value;
+            obj.password=pwd.value;
+
+            ajax.register(obj);
+        }
     });
 
     e.target.classList.add('active');
@@ -337,12 +436,11 @@ signUpBtn.addEventListener('click',e=>{
             gmailSignUp.classList.remove('opacity-0');
         }, 0);
     },400);
+    
 });
 
 //signUp
 //upload pic
-const profileImage=document.getElementById('profileImage'); //input
-let profilePhoto=document.querySelector('.profilePhoto'); //image
 profileImage.addEventListener('change',e=>{
     let reader=new FileReader();
     reader.onload=(e)=>{
@@ -352,5 +450,21 @@ profileImage.addEventListener('change',e=>{
 });
 
 memberSignUpSubmit.addEventListener('click',e=>{
+    e.preventDefault();
+    e.stopPropagation();
     signUpValidation();
 });
+memberIndexCancel.addEventListener('click',e=>{
+    account.value='';
+    pwd.value='';
+    //https://bftsai.github.io/outfitpals/index.html
+    location.href='https://bftsai.github.io/outfitpals/index.html';
+});
+// ajax.delete(2)
+// axios.post(`${apiUrl}/register`,{
+//     email: 'test@gamil.com',
+//     "password": '123456789'
+// })
+//     .then(res=>{
+//         console.log(res);
+//     })
--- assets/js/member_googleAPI.js
+++ assets/js/member_googleAPI.js
@@ -1,21 +1,3 @@
-const account=document.getElementById('account');
-// 登入之後
-function onSignIn(response){
-    var base64Url = response.credential.split('.')[1];
-    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
-    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
-    console.log(response);
-    console.log(jsonPayload);
-    console.log(JSON.parse(jsonPayload).sub);
-    window.localStorage.token=response.credential;
-    account.value=JSON.parse(jsonPayload).sub;
-};
-// console.log(window.localStorage);
-
-
-
 /*
     JWT 線上編譯網站
     https://jwt.io/#debugger
@@ -29,5 +11,120 @@ function onSignIn(response){
     npm install jwt-decode 專門解碼 JWT (待驗證)
     https://www.npmjs.com/package/jwt-decode
 */
+//axios
+//json-server
+const apiUrl='http://localhost:3000/';
+//member pages
+const memberIndex=document.querySelector('.member-index');
+const memberSignUpData=document.querySelector('.member-signUpData');
+//signUp index
+//signIn signUp Btn
+const signInBtn=document.querySelector('.signInBtn');
+const signUpBtn=document.querySelector('.signUpBtn');
+const account=document.getElementById('account');
+const pwd=document.getElementById('pwd');
+//member signUp input
+const profileImage=document.getElementById('profileImage'); //input
+let profilePhoto=document.querySelector('.profilePhoto'); //image
+const signUpPwd=document.getElementById('signUpPwd');
+const signUpMail=document.getElementById('signUpMail');
+const signUpName=document.getElementById('signUpName');
+const signUpNickName=document.getElementById('signUpNickName');
+const signUpBirth=document.getElementById('signUpBirth');
+const signUpTel=document.getElementById('signUpTel');
+const signUpMale=document.getElementById('signUpMale');
+const signUpFemale=document.getElementById('signUpFemale');
+const signUpReservationTime=document.getElementById('signUpReservationTime');
+const signUpReservationLocation=document.getElementById('signUpReservationLocation');
+const signUpHeight=document.getElementById('signUpHeight');
+const signUpWeight=document.getElementById('signUpWeight');
+const signUpPopArea=document.getElementById('signUpPopArea');
+const signUpStyle=document.getElementById('signUpStyle');
+const signUpOutfitPrice=document.getElementById('signUpOutfitPrice');
+const signUpLoveStore=document.getElementById('signUpLoveStore');
+const signUpIntroduce=document.getElementById('signUpIntroduce');
+//ajax
+const ajax={
+    async register(obj){
+        try {
+            const register=await axios.post(`${apiUrl}register`,obj);
+            localStorage.outfitpalsToken=register.data.accessToken;
+            if(register.status===201){
+                signUpMail.value=obj.email;
+                signUpPwd.value=obj.password;
+                profilePhoto.setAttribute('src',obj.image);
+                signUpName.value=obj.name;
+                signUpNickName.value=obj['nick name'];
+
+                memberIndex.classList.add('opacity-0');
+                setTimeout(() => {
+                    account.value='';
+                    pwd.value='';
+                    memberIndex.classList.add('d-none');
+                    memberSignUpData.classList.remove('d-none');
+                    setTimeout(() => {
+                        memberSignUpData.classList.remove('opacity-0')
+                    }, 0);
+                }, 400);
+            }
+        } catch (err) {
+            console.log(err);
+            alert('google 帳號已被使用');
+        }
+    },
+    async patchUsers(obj){
+        let id=0;
+        const getUsers=(await axios.get(`${apiUrl}users`)).data;
+        getUsers.forEach(item=>{
+            if(item.email===obj.email){
+                id=item.id;
+            }
+        });
+        const patchUsers=await axios.patch(`${apiUrl}users/${id}`,obj)
+    },
+    async signIn(){
+        const signIn=await axios.post(`${apiUrl}signin`,obj);
+    },
+    async delete(id){
+        try {
+            const deleted=await axios.delete(`${apiUrl}users/${id}`);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+};
+// Google third party login
+function onSignIn(response){
+    var base64Url = response.credential.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+    console.log(response);
+    console.log(JSON.parse(jsonPayload));
+    window.localStorage.googleToken=response.credential;
+
+    if(signInBtn.className.includes('active')){
+
+    }else{
+        let signObj={};
+        signObj.email=JSON.parse(jsonPayload).email;
+        signObj.password=`Google${JSON.parse(jsonPayload).sub}`;
+        signObj.image=JSON.parse(jsonPayload).picture;
+        signObj.name=JSON.parse(jsonPayload).name;
+        signObj['nick name']=JSON.parse(jsonPayload).given_name;
+
+        ajax.register(signObj);
+    }
+};
+const gmailSignUp=document.querySelector('.gmailSignUp');
+gmailSignUp.addEventListener('click',e=>{
+    console.log('gmailSignUp');
+})
+
+
+
+//console.log(signInBtn.className.includes('active'));
+
 
 
