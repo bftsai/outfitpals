2f7c375f78be904083235c0251d36523c590fe52
bf_tsai <tsai19911026@gmail.com>
2023-11-22 03:15:21 +0800

    fix: 🐛 google login

--- assets/js/member_googleAPI.js
+++ assets/js/member_googleAPI.js
@@ -624,7 +624,6 @@ function onSignIn(response){
     var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
     return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
     }).join(''));
-    console.log(response);
     
     document.cookie=`googleToken=${response.credential}`;
 
