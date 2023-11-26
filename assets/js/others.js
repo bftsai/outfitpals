e022fc5b7997bfa5f1a4b6ba0cc0a26a8d7b2c03
eric <26416387.re@gmail.com>
2023-11-26 22:21:37 +0800

    js

--- assets/js/others.js
+++ assets/js/others.js
@@ -12,6 +12,17 @@ const months = document.querySelector("#months")
 const td = document.querySelectorAll("td")
 
 
+const checkboxElements = document.querySelectorAll('.checkbox');
+const ctn = document.querySelector(".ctn")
+const pos = document.querySelector(".pos")
+
+
+const go = document.querySelector("#go")
+const doublecheck = document.querySelector(".doublecheck")
+const text = document.querySelector(".text")
+const ans = document.querySelector(".ans")
+const ans3 = document.querySelector(".ans3")
+
 for (let i = 0; i < thumbLinks.length; i++) {
     
     thumbLinks[i].addEventListener("click", function (event) {
@@ -73,29 +84,49 @@ td.forEach(function(td,index){
 
 
 
-const go = document.querySelector("#go")
-
-let alltoala = []
-
-go.addEventListener("click",function(){
-    
-    td.forEach(function(o){
-       
-        if(o.classList.contains("go")){
-            const row = o.getAttribute('data-row');
-            const col = o.getAttribute('data-col');
-            let dat = {
-                "year":year,
-                "month":month,
-                "Week":col,
-                "date":row
-            }
-            alltoala.push(dat)
-        // axios.post("http://localhost:3000/reserve" , dat)     
+ctn.addEventListener("click",function(){
+    pos.innerHTML = '';
+    ans3.innerHTML = '';
+    text.value = '';
+    checkboxElements.forEach(function (checkbox, index) {
+        if (checkbox.checked) {
+            var labelElement = document.querySelector('.d' + (index + 1));
+            var labelText = labelElement.innerText.trim();
+            pos.innerHTML += `<li>${labelText}</li>`;
+            ans3.innerHTML += `<li>${labelText}</li>`
         }
-    })
-    
+    });
     
 })
 
+doublecheck.addEventListener("change",function(){
+    go.disabled = !doublecheck.checked;
+})
+
+let alltoala = []
+go.addEventListener("click",function(){
+    
+    // td.forEach(function(o){
+       
+    //     if(o.classList.contains("go")){
+    //         const row = o.getAttribute('data-row');
+    //         const col = o.getAttribute('data-col');
+    //         let dat = {
+    //             "year":year,
+    //             "month":month,
+    //             "Week":col,
+    //             "date":row
+    //         }
+    //         alltoala.push(dat)
+    //         alltoala.push(dat)
+    //         alltoala.push(dat)
+    //         alltoala.push(dat)
+       
+    //     }
+    // })
+    let want = text.value
+    ans.innerHTML = `<p>${want}</p>`
+})
+ // axios.post("http://localhost:3000/reserve" , alltoala)     
 console.log(alltoala)
+
