f8ee9ee8d34e6cfe9cacf3f3934054784ce14add
Eric <26416387.re@gmail.com>
2023-12-11 00:27:34 +0800

    feat: 🎸 渲染資訊頁面

--- assets/js/information.js
+++ assets/js/information.js
@@ -16,53 +16,63 @@ const like = document.querySelector(".like");
 const favorite = document.querySelector(".favorite");
 
 
-function getData() {
+const postrId = location.href.split("=")[1];
 
-    axios.get('http://localhost:3000/posts/1?_expand=user')
+        axios.get(`http://localhost:3000/posts/${postrId}?_expand=user`)
         .then(res => {
-            let data = res.data;
-            console.log(data);
-            renderData(data);
+            posterNickName.textContent = res.data.user.name;
+            posterIntroduce.textContent = res.data.user.introduce;
+            posterInfo.innerHTML = `<p class="posts__user__height ps-3 fs-9 fs-lg-7">${res.data.user.height}cm ${res.data.user.weight}kg</p>`;
+            outfitStyle.textContent = res.data.outfitStyle;
+            postImg.innerHTML = `<img src=${res.data.imgUrl} style="width: 500px; height: 440px;"
+            class="postImg object-fit-cover">`;
+            postTitle.textContent = res.data.title;
+            postContent.textContent = res.data.body;
+            posterMore.innerHTML = `<button type="button" class=" btn btn-primary fs-3 arrowBtn2">看更多${res.data.user.name}的穿搭</button>`
+            posterOther.innerHTML = `<h2 class="text-center ">查看${res.data.user.name}的其他搭配</h2>`
+            const sty = document.querySelector(".sty")
+            const stys = res.data.brands;
+               if(stys.length <2){
+                   sty.innerHTML =` <p class="fs-3 d-flex justify-content-center ms-3">服飾品牌</p>
+                   <div class="d-flex justify-content-center">
+                       <button type="button" class="btn btn-primary btn-pill  ms-3">${stys[0]}</button>
+                   </div>`
+                   
+               }else{
+                   
+                   sty.innerHTML = ` <p class="fs-3 d-flex justify-content-center ms-3">服飾品牌</p>
+                   <div class="d-flex justify-content-center">
+                       <button type="button" class="btn btn-primary btn-pill ms-3">${stys[0]}</button>
+                   </div>        
+                   <button type="button" class="btn btn-primary btn-pill mt-3 ms-3">${stys[1]}</button>`
+   
+               }
+               let user = res.data.userId
+               const reserve = document.querySelector(".reserve")
+               reserve.addEventListener("click",function(){
+                    window.location.href = "http://localhost:5173/outfitpals/pages/others.html?userId=" + user + "&page=1";
+               })
+
+               function showMore() {
+                const longText = document.getElementById('longText');
+                longText.style.maxHeight = 'none';
+              }
+
         })
-        .catch(error => {
-            console.log(error.response);
-        });
-}
+        
 
-function renderData(data) {
-    posterNickName.textContent = data.user[`nick name`];
-    posterIntroduce.textContent = data.user.introduce;
-    posterInfo.innerHTML = `<p class="posts__user__height ps-3 fs-9 fs-lg-7">${data.user.height}cm ${data.user.weight}kg</p>`;
-    outfitStyle.textContent = data.outfitStyle;
-    postImg.innerHTML = `<img src=${data.imgUrl} style="width: 500px; height: 440px;"
-    class="postImg object-fit-cover">`;
-    console.log(postImg);
-    
-    postTitle.textContent = data.title;
-    postContent.textContent = data.body;
-    posterMore.innerHTML = `<button type="button" class=" btn btn-primary fs-3 arrowBtn2">看更多${data.user[`nick name`]}的穿搭</button>`
-    posterOther.innerHTML = `<h2 class="text-center ">查看${data.user[`nick name`]}的其他搭配</h2>`
 
-    // 品牌渲染（最多兩個排版較合適）
-    let str = '<p class=" fs-5 fs-lg-2 text-nowrap">服飾品牌</p>';
-    data.brands.forEach(item => {
-        str += `<button type="button" class="brands btn btn-primary btn-pill">${item}</button>`;
-        console.log(str);
-    });
-    brands.innerHTML = str;
-}
-// getData();
 
 // 測試收藏功能
-function favoriteData() {
+// function favoriteData() {
 
-    axios.get('http://localhost:3000/favorites/2?_expand=user&_expand=post')
-        .then(res => {
-            let data = res.data;
-            console.log(data);
-        })
-        .catch(error => {
-            console.log(error.response);
-        });
-}
-favoriteData()
+//     axios.get('http://localhost:3000/favorites/2?_expand=user&_expand=post')
+//         .then(res => {
+//             let data = res.data;
+//             console.log(data);
+//         })
+//         .catch(error => {
+//             console.log(error.response);
+//         });
+// }
+// favoriteData()
