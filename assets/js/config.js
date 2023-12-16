106671641ccf5a4f12817fe8b6708ca94064170a
oria <xzxz753951@gmail.com>
2023-12-16 10:43:36 +0800

    docs: ✏️ 新增假資料

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,9 +1,9 @@
-// export const locationUrl='https://bftsai.github.io/outfitpals/';
-export const locationUrl='http://localhost:5173/outfitpals/pages/';
+export const locationUrl='https://bftsai.github.io/outfitpals/';
+// export const locationUrl='http://localhost:5173/outfitpals/pages/';
 
 // export  const apiUrl='http://localhost:3000/';
 export const apiUrl='https://outfitpals-web-server.onrender.com/'; //render server
 
 
-// export const localUrl='https://bftsai.github.io/outfitpals';
-export const localUrl='http://localhost:5173/outfitpals/pages';
+export const localUrl='https://bftsai.github.io/outfitpals';
+// export const localUrl='http://localhost:5173/outfitpals/pages';
